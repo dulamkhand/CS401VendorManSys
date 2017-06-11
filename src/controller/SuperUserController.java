fed42980c49124e53fb2121b40784ae08b8dba72
arginbek <arginbek@gmail.com>
2017-06-11 12:17:50 -0500

    SuperUser logout added

--- src/controller/SuperUserController.java
+++ src/controller/SuperUserController.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -16,6 +17,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -24,6 +27,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import model.account.AccountEmployeeVendorDAO;
 import model.account.EmployeeResult;
 import util.DBUtil;
@@ -183,4 +187,12 @@ public class SuperUserController implements Initializable {
         }
     }
 
+    @FXML
+    private void logout(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/Login.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
